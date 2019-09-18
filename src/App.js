179e37cb4e4f1ddcb59db9773d0c4b62d1c57a75
Benjamin Haines <bhaines@yext.com>
2019-09-18 14:55:30 -0400

    Add search bar for filtering items

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 
 class TimeLogger extends Component {
     state = {
-      reminderInterval: "30"
+      reminderInterval: "30",
+      itemFilter: '',
     }
     onChange = e => {
       this.setState({ [e.target.name]: e.target.value });
       window.timer.set_interval(parseFloat(e.target.value))
     }
+
+    onFilterChange = e => {
+      this.setState({itemFilter: e.target.value});
+    }
+
     render() {
+        const issues = this.state.itemFilter ? this.props.issues.filter(issue => issue.toLowerCase().includes(this.state.itemFilter.toLowerCase())) : this.props.issues;
         const {
           reminderInterval,
         } = this.state;
         return (
           <div style={{display:"flex", flexDirection: "column"}}>
+            <input onChange={this.onFilterChange} type="text"></input>
             <select style={{height:"200px"}} multiple>
-                {this.props.issues.map(function(name, index){
+                {issues.map(function(name, index){
                     return <option key={ index }>{name}</option>;
                   })}
             </select>
